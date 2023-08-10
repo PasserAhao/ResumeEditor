ca7f673dd3b6e0c5e0fe6b79ade4abc789f73420
薄年阿豪 <11384280+bonianahao@user.noreply.gitee.com>
2023-08-10 22:24:43 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   // outputDir:'./dist/resume',
   // publicPath:'/EditResume/',
   outputDir:'./Editor',
-  publicPath:'./'
+  publicPath:'./',
+  assetsDir:'./'
 })
 
